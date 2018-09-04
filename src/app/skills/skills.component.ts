4ff5492c1f08f60d1b17d7ab04eef3eb49aef296
Trent <mr.peschke@gmail.com>
2018-09-04 15:04:23 -0600

    got projects skeleton worked out

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillsComponent implements OnInit {
 
-  public skills = [{image: require("../../images/javascript.svg"), name: "Javascript ES6"}, {image: require("../../images/react.svg"), name: "React"}, {image: require("../../images/redux.svg"), name: "Redux"}, {image: require("../../images/node.png"), name: "Node.js"}, {image: require("../../images/express.png"), name: "express"}, {image: require("../../images/socket-io.svg"), name: "socket.io"}, {image: require("../../images/jest.svg"), name: "jest"}, {image: require("../../images/api.svg"), name: "Restful API"}]
-
+  
   constructor() { }
-
+  
   ngOnInit() {
   }
+  
+  public skills = [{image: require("../../images/javascript.svg"), name: "Javascript ES6"}, {image: require("../../images/react.svg"), name: "React"}, {image: require("../../images/redux.svg"), name: "Redux"}, {image: require("../../images/node.png"), name: "Node.js"}, {image: require("../../images/express.png"), name: "express"}, {image: require("../../images/socket-io.svg"), name: "socket.io"}, {image: require("../../images/jest.svg"), name: "jest"}, {image: require("../../images/api.svg"), name: "Restful API"}]
 
 }
