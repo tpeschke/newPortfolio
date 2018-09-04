f9cef593def14ea947f7f9bed633ae09c9c01ef0
Trent <mr.peschke@gmail.com>
2018-09-04 10:35:07 -0600

    did the skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderbarComponent } from './headerbar/headerbar.component';
+import { TitleComponent } from './title/title.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderbarComponent,
+    TitleComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
